4cb809b52a0ee37baf0806f71debd03d591dbb71
Yash <63893378+YSBoomOfficial@users.noreply.github.com>
2023-02-22 16:34:15 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UIKitStackBuilder
+
+A description of this package.
---
+++ Sources/UIKitStackBuilder/UIKitStackBuilder.swift
@@ -0,0 +1,6 @@
+public struct UIKitStackBuilder {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
